912ea412f0d9dab35e5f4b7494698d7ef9191e80
Pato Ezequiel <pato.ezequiel@gmail.com>
2019-05-02 03:08:13 -0300

    Crear reglas de TSLint y ajustar archivos a ellas

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
-import Vue from 'vue';
-import './plugins/vuetify';
+import Vue, { VNode, CreateElement } from 'vue';
 import App from './App.vue';
+import './plugins/vuetify';
 import router from './router';
 import store from './store';
 
 Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  store,
-  render: (h) => h(App),
+	router,
+	store,
+	render: (h) => h(App),
 }).$mount('#app');
--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,12 +1,16 @@
-import { shallowMount } from '@vue/test-utils';
 import HelloWorld from '@/components/HelloWorld.vue';
+import { shallowMount } from '@vue/test-utils';
+import Vue from 'vue';
+import Vuetify from 'vuetify';
+
+Vue.use( Vuetify );
 
 describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message';
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg },
-    });
-    expect(wrapper.text()).toMatch(msg);
-  });
+	it('renders props.msg when passed', () => {
+		const msg: string = 'new message';
+		const wrapper = shallowMount(HelloWorld, {
+			propsData: { msg },
+		});
+		expect(wrapper.text()).toMatch(msg);
+	});
 });
