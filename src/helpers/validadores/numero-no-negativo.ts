9a22f1dfdb8572fb8906542b03b61a08cbb4b9c8
Pato Ezequiel <pato.ezequiel@gmail.com>
2019-05-19 00:34:15 -0300

    Cambiar mensaje de error en validador de campo no negativo

--- src/helpers/validadores/numero-no-negativo.ts
+++ src/helpers/validadores/numero-no-negativo.ts
@@ -13,7 +13,7 @@ export const numeroNoNegativo = ( nombreDelCampo: string ) => ( valor: unknown )
 	const patronDeNumero: RegExp = /^\s*(?:\+|\-)?[0-9]+(?:(?:\,|\.)[0-9]+)?\s*$/;
 
 	if ( typeof valor === 'string' && ! patronDeNumero.test( valor ) ) {
-		return `El campo ${ nombreDelCampo } no es un número`;
+		return `El campo "${ nombreDelCampo }" no es un número`;
 	}
 
 	const valorNumerico: number = Number.parseFloat( valor.toString( ) );
