318a6ca0bfbf5b0f7985cdca082a1c6c9d0738ed
Armando <armandomarino123@gmail.com>
2019-06-06 00:35:07 -0300

    Se agrego un poco de texto para rellenar las suscripciones

--- src/views/AdministracionDeSuscripciones.vue
+++ src/views/AdministracionDeSuscripciones.vue
@@ -1,3 +1,22 @@
 <template>
-	<p1> Hello! </p1>
+	<v-layout class="justify-center">
+		<v-card class="text-xs-center texto">
+			<v-card-text>
+				Precio actual de la Suscripcion Premium:
+			</v-card-text>
+
+			<v-card-text>
+				Precio actual de la Suscripcion Regular:
+			</v-card-text>
+
+			<v-btn>Modificar precio de la suscripción regular</v-btn>
+			<v-btn>Modificar precio de la suscripción premium</v-btn>
+		</v-card>
+	</v-layout>
 </template>
+
+<style lang="stylus" scoped>
+	.texto {
+		font-size: 30px;
+	}
+</style>
